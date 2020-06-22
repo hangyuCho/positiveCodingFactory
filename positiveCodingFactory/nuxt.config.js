099e44705bcfde35e2f570f657b003dad1d85f0c
hangyu cho <chg3376@gmail.com>
2020-06-23 01:22:44 +0900

    dotEnv설정

--- positiveCodingFactory/nuxt.config.js
+++ positiveCodingFactory/nuxt.config.js
@@ -65,7 +65,7 @@ export default {
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
-    baseURL: 'https://onestarweblog.com/osb',
+    baseURL: process.env.API_URL,
     proxyHeaders: false,
     credentials: false
   },
